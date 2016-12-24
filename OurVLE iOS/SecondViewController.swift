aaef513264404bcc532159f5a9493750ae7c4b20
Brandon Hinds <brandonhinds@Jasons-MacBook-Air.local>
2016-12-23 22:46:16 -0500

    Initial Commit

---
+++ OurVLE iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OurVLE iOS
+//
+//  Created by Brandon Hinds on 12/23/16.
+//  Copyright © 2016 Brandon Hinds. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
